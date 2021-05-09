60d2cea66e438fc68a7ff4dd36377dd3b7eff500
Graham Bleaney <gbleaney@fb.com>
2021-05-09 16:26:04 -0400

    Update sink dump script and regenerate sink list

--- scripts/generate_sink_list.py
+++ scripts/generate_sink_list.py
@@ -8,5 +8,6 @@ from code_execution.execution_base import get_exploits, ExploitEncoder
 
 all_exploits = get_exploits(exclude_abstract=False)
 exploits_with_function = [exploit for exploit in all_exploits if exploit.vulnerable_function]
+exploits_with_function.sort(key=lambda exploit: (exploit.category_name, exploit.get_vulnerable_function_fqn()))
 
 print(ExploitEncoder(indent=2).encode(exploits_with_function))
